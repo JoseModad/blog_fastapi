c6c6dadc6166c440cff67834d0948dd2410ae146
turko2373 <turko1073@gmail.com>
2023-01-02 13:28:05 -0300

    hash user password

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,12 +1,35 @@
 # Fastapi
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+
+# Intern Modules
+
+from ..schemas import User, db
+from ..utils import get_password_hash
+
+
 
 router = APIRouter(
     tags = ["User Routes"]
 )
 
 
-@router.get("/")
-def get():
-    return {"msg": "Hello World"}
+@router.post("/registration", response_description = "Register a user")
+async def registration(user_info: User):
+    user_info = jsonable_encoder(user_info)
+    
+    # check for duplication
+    
+    username_found = await db["users"].find_one({"name": user_info["name"]})
+    email_found = await db["users"].find_one({"email": user_info["email"]})
+    
+    if username_found:
+        raise HTTPException(status_code = status.HTTP_409_CONFLICT, detail = "Username is already taken")
+    
+    if email_found:
+        raise HTTPException(status_code = status.HTTP_409_CONFLICT, detail = "Email id already taken")
+    
+    # Hash user password
+    
+    user_info["password"] = get_password_hash(user_info["password"])
--- api/schemas.py
+++ api/schemas.py
@@ -5,6 +5,10 @@ import motor.motor_asyncio
 from dotenv import load_dotenv
 from bson import ObjectId
 
+# Pydantic
+
+from pydantic import BaseModel, Field, EmailStr
+
 # Load env
 
 load_dotenv()
@@ -16,7 +20,7 @@ db = client.blog_api
 
 # Bson to fastapi JSON
 
-class PyObjetcIs(ObjectId):
+class PyObjetcId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
@@ -30,3 +34,22 @@ class PyObjetcIs(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type = "string")
+        
+        
+class User(BaseModel):
+    id: PyObjetcId = Field(default_factory = PyObjetcId, alias = "_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+    
+    class Config:
+        allowed_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jhon Doe",
+                "email": "jdoe@example.com",
+                "password": "secret-code"
+            }
+        }
